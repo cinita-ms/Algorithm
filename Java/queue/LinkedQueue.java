c98622e922a5a300fcaee0192c3e4b107060d475
cinita <cinita.lv@gmail.com>
2019-01-15 00:30:22 +0800

    add linked queue

--- Java/queue/LinkedQueue.java
+++ Java/queue/LinkedQueue.java
@@ -1,12 +1,27 @@
 public class LinkedQueue<E> implements Queue<E> {
 
+    private Node<E> head;
+    private Node<E> tail;
+
     @Override
     public boolean enqueue(E item) {
-        return false;
+        Node<E> node = new Node<>();
+        if (head == null) {
+            head = node;
+        }
+        node.data = E;
+        tail.next = node;
+        tail = node;
+        return true;
     }
 
     @Override
     public E dequeue() {
-        return null;
+        if (head == null) {
+            return null;
+        }
+        E item = head.data;
+        head = head.next;
+        return item;
     }
 }
