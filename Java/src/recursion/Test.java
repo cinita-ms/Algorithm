fba50bba875ced26cf956539a8e0c846a6dc54f5
cinita <cinita.lv@gmail.com>
2019-02-16 22:10:49 +0800

    update recursion

---
+++ Java/src/recursion/Test.java
@@ -0,0 +1,9 @@
+package recursion;
+
+class Test {
+
+    public static void main(String[] args) {
+        WalkStep w = new WalkStep();
+        System.out.printf("getStep = %d\n", w.getStep(8));
+    }
+}
--- Java/src/recursion/WalkStep.java
+++ Java/src/recursion/WalkStep.java
@@ -1,3 +1,5 @@
+package recursion;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -5,7 +7,7 @@ public class WalkStep {
 
     private Map<Integer, Integer> results = new HashMap<>();
 
-    public int f1(int n) {
+    public int getStep(int n) {
         if (n == 1) {
             return 1;
         }
@@ -18,34 +20,8 @@ public class WalkStep {
             return results.get(n);
         }
 
-        int result = f1(n - 1) + f1(n - 2);
+        int result = getStep(n - 1) + getStep(n - 2);
         results.put(n, result);
         return result;
-    }
-
-    public int f2(int n) {
-        if (n == 1) {
-            return 1;
-        }
-
-        if (n == 2) {
-            return 2;
-        }
-
-        int pre = 2;
-        int prepre = 1;
-        int result = 0;
-        for (int i = 3; i <= n; i++) {
-            result = pre + prepre;
-            prepre = pre;
-            pre = result;
-        }
-        return result;
-    }
-
-    public static void main(String[] args) {
-        WalkStep w = new WalkStep();
-        System.out.printf("f1 = %d\n", w.f1(8));
-        System.out.printf("f2 = %d\n", w.f2(8));
     }
 }
