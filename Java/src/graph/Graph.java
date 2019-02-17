8abfa5d0d2f135a09cb3a46acecadc530afef44f
cinita <cinita.lv@gmail.com>
2019-02-18 00:06:37 +0800

    graph part 1

---
+++ Java/src/graph/Graph.java
@@ -0,0 +1,22 @@
+package graph;
+
+import java.util.LinkedList;
+
+public class Graph {
+
+    private int v;
+    private LinkedList<Integer>[] adj;
+
+    public Graph(int v) {
+        this.v = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; ++i) {
+            adj[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge(int s, int t) {
+        adj[s].add(t);
+        adj[t].add(s);
+    }
+}
