d46517097b9647d1a54513782718f2b89338576a
cinita <cinita.lv@gmail.com>
2019-07-03 00:05:52 +0800

    [70, 101, 104, 121, 136]

--- Java/src/leetcode/Solution.java
+++ Java/src/leetcode/Solution.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+import base.BTNode;
 import base.Node;
 import base.NodeInt;
 
@@ -210,6 +211,114 @@ public class Solution {
 
     // 70. 爬楼梯
     public static int climbStairs(int n) {
-        return -1;
+        if (n == 1) {
+            return 1;
+        }
+
+        if (n == 2) {
+            return 2;
+        }
+
+        return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+
+    public static int climbStairs_1(int n) {
+        int[] status = new int[n + 1];
+        status[1] = 1;
+        status[2] = 2;
+
+        for (int i = 3; i <= n; ++i) {
+            status[n] = status[n - 1] + status[n - 2];
+        }
+
+        return status[n];
+    }
+
+    // 101. 对称二叉树
+    public static <E> boolean isSymmetric(BTNode<E> root) {
+        return isMirror(root, root);
+    }
+
+    private static <E> boolean isMirror(BTNode<E> root1, BTNode<E> root2) {
+        if (root1 == null && root2 == null) {
+            return true;
+        }
+
+        if (root1 == null || root2 == null) {
+            return false;
+        }
+
+        return root1.data == root2.data
+                && isMirror(root1.left, root2.right)
+                && isMirror(root1.right, root2.left);
+    }
+
+    // 104. 二叉树的最大深度
+    public static <E> int maxDepth(BTNode<E> root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    // 121. 买卖股票的最佳时机
+    public static int maxProfit(int[] prices) {
+        if (prices == null || prices.length < 2) {
+            return 0;
+        }
+
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
+            }
+        }
+
+        return maxProfit;
+    }
+
+    // 136. 只出现一次的数字
+    public static int singleNumber(int[] nums) {
+        // a ^ 0 = a
+        // a ^ a = 0
+        // a ^ b ^ a = a ^ a ^ b = 0 ^ b = b
+
+        if (nums == null || nums.length < 1) {
+            throw new IllegalArgumentException("Unsupported param.");
+        }
+
+        int r = nums[0];
+        for (int i = 1, len = nums.length; i < len; ++i) {
+            r ^= nums[i];
+        }
+
+        return r;
+    }
+
+    public static int singleNumber_1(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                set.remove(num);
+            } else {
+                set.add(num);
+            }
+        }
+
+        Iterator<Integer> iterator = set.iterator();
+        if (iterator.hasNext()) {
+            return iterator.next();
+        } else {
+            throw new IllegalArgumentException("Unsupported param.");
+        }
+    }
+
+    public static int singleNumber_2(int[] nums) {
+        // 排序和遍历
+        return 0;
     }
 }
