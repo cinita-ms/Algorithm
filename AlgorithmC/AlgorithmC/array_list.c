a899f1548648e0c374282684dfb71538e8f09366
cinita <cinita@yeah.net>
2021-05-27 12:35:07 +0800

    Basic C project

--- AlgorithmC/AlgorithmC/array_list.c
+++ AlgorithmC/AlgorithmC/array_list.c
@@ -2,7 +2,7 @@
 //  array_list.c
 //  AlgorithmC
 //
-//  Created by Zhiyuan Lv on 2021/5/26.
+//  Created by Zhiyuan Lv on 2021/5/27.
 //
 
 #include "array_list.h"
--- AlgorithmC/AlgorithmC/array_list.h
+++ AlgorithmC/AlgorithmC/array_list.h
@@ -2,20 +2,12 @@
 //  array_list.h
 //  AlgorithmC
 //
-//  Created by Zhiyuan Lv on 2021/5/26.
+//  Created by Zhiyuan Lv on 2021/5/27.
 //
 
 #ifndef array_list_h
 #define array_list_h
 
 #include <stdio.h>
-
-typedef void *ArrayListValue;
-
-typedef struct
-{
-
-} ;
-
 
 #endif /* array_list_h */
